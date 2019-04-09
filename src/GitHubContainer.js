df1fbf822b07ae2b1110674742ac911af9559388
Aspen James <aspenjames@tqca.org>
2019-04-08 22:06:17 -0700

    initial commit

---
+++ src/GitHubContainer.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class GitHubContainer extends Component {
+  render() {
+    return (
+      <div>GitHub Issue Form + Response goes here</div>
+    );
+  }
+}
+
+export default GitHubContainer;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import GitHubContainer from './GitHubContainer';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<GitHubContainer />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
