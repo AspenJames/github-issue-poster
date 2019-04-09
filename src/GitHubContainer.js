28fe3c36e1fa542fd839b4070495c266716b3c2c
Aspen James <aspenjames@tqca.org>
2019-04-09 15:11:57 -0700

    end of demo

--- src/GitHubContainer.js
+++ src/GitHubContainer.js
@@ -16,11 +16,12 @@ class GitHubContainer extends Component {
   }
 
   submitForm = (data) => {
-    const url = `https://api.github.com/repos/${/* repo goes here */}/issues`;
+    const url = `https://api.github.com/repos/${data.repo}/issues`;
 
     // build our body of the request
     const postData = {
-      // let's set this up
+      title: data.title,
+      body: data.body
     };
 
     // reset state to clear Results component
@@ -31,19 +32,31 @@ class GitHubContainer extends Component {
     
     // so fetch
     fetch(url, {
+      method:  "POST",
       headers: {
         'Authorization': `token ${api_key}`,
         'Accept': 'application/vnd.github.v3+json',
         'Content-Type': 'applicaton/json'
       },
-      body: ''// use postData from above
+      body: JSON.stringify(postData)
     }).then(res => res.json())
       .then(json => {
         if (json.body) {
+          this.setState({
+            result: {
+              title: json.title,
+              html_url: json.html_url,
+              body: json.body
+            }
+          });
           // if we posted the issue, set the state for "result"
           // we need a few pieces of data from the result returned:
           // issue title, issue body, and html_url
         } else {
+
+          this.setState({
+            error:  "whoopsie"
+          })
           // set the state for "error"
           // doesn't really matter how for our basic example
         }
--- src/IssueForm.js
+++ src/IssueForm.js
@@ -1,12 +1,52 @@
 import React, { Component } from 'react';
 
 class IssueForm extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      repo: '',
+      title: '',
+      body: ''
+    }
+  }
+
+
+  onChange = (ev) => {
+    this.setState({
+      [ev.target.name]: ev.target.value
+    })
+  }
+
+  handleSubmit = (ev) => {
+    ev.preventDefault();
+    this.props.submitForm(this.state)
+  }
   
   render() {
     return (
       <React.Fragment>
-        <p>This is where our form goes!</p>
-        <p>We need fields for repo, issue title, and issue body</p>
+        <form onSubmit={this.handleSubmit}>
+          <label>Repo:</label>
+          <input type='text'
+            name='repo'
+            onChange={this.onChange}
+            value={this.state.repo} />
+
+          <label>Title:</label>
+          <input type='text'
+            name='title'
+            onChange={this.onChange}
+            value={this.state.title} />
+
+          <label>Body:</label>
+          <input type='text'
+            name='body'
+            onChange={this.onChange}
+            value={this.state.body} />
+
+          <input type='submit' value="Post Issue" />
+        </form>
       </React.Fragment>
     )
   }
